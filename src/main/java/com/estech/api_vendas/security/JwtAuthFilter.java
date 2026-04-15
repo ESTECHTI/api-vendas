21bee561a9e97b5775cd4cc161580a20ba585f48
ESTECHTI <e.s.tech@outlook.com>
2026-04-14 21:35:09 -0300

    Desenvolvendo a verificação se existe o bearer token

--- src/main/java/com/estech/api_vendas/security/JwtAuthFilter.java
+++ src/main/java/com/estech/api_vendas/security/JwtAuthFilter.java
@@ -6,4 +6,25 @@ import com.estech.api_vendas.security.JwtService;
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+
+    public JwtAuthFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    protected void doFilterInternal(
+        HttpServeletRequest request,
+        HttpServeletResponse response,
+        FilterChain filterChain
+    ) throws ServeletException, IOException {
+
+        String authHeader = request.getHeader("Authoriation");
+
+        String token = null;
+        String email = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            email = jwtService.extrairEmail(token);
+        }
+    }
 }
--- src/main/java/com/estech/api_vendas/security/JwtService.java
+++ src/main/java/com/estech/api_vendas/security/JwtService.java
@@ -2,4 +2,8 @@ package com.estech.api_vendas.security;
 
 public class JwtService {
 
+    public String extrairEmail(String token) {
+        // Lógica para extrair o email do token JWT
+        return null; // Retornar o email extraído do token
+    }
 }
